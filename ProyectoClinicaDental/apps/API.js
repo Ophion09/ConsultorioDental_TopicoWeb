ac85acb8c6805eabceb6bc25e91cd1da6469559f
Jose Miguel <migueloz60@outlook.com>
2023-11-25 00:07:37 -0600

    UPDATE: agregacion de interfaz de usuarios

--- ProyectoClinicaDental/apps/API.js
+++ ProyectoClinicaDental/apps/API.js
@@ -22,7 +22,7 @@ export const login = async user => {
             // enviar el token al mismo usuario correspondiente en la bd
             console.log(user);
         userLoging = user;
-        window.location.href = '../views/administration.html';
+        window.location.href = '../views/user.html';
 
         return user;
         } else {
@@ -564,5 +564,119 @@ export const updateProcedureById = async (user, id, updateData) => {
     return data;
   } catch (error) {
     console.log(error);
+  }
+}
+
+export const getUsers = async user => {
+  const {email, token} = user;
+  console.log(token);
+  const main = document.querySelector('#main');
+  try {
+    const response = await fetch(`${url}/users`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Authorization': `${token}`
+      }
+    });
+    
+    const data = await response.json();
+    if(response.status === 401) {
+      console.log('Token invalido');
+      showAlert('Sesion Expirada', 'error', main);
+       setTimeout(() => {
+         window.location.href = '../views/login.html';
+       }, 3000);
+      return;
+    } else {
+      return data;
+      // Si todo sale bien, regresa data, que es la respuesta obtenida del fetch pero convertida a json
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+    //showAlert('Servidor Caido', 'ERROR', formulario)
+    return;
+  }
+
+}
+
+export const deleteUser = async (user, idUser) => {
+  const {email, token} = user;
+  try {
+    const response = await fetch(`${url}/users/${idUser}`, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+          'Authorization': `${token}`
+      },
+      body: JSON.stringify(idUser)
+    });
+    const data = await response.json();
+    if(response.ok) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
+
+export const postUser = async (dataUser,user) => {
+  console.log(user);
+  const {email, token} = dataUser;
+  try {
+    const response = await fetch(`${url}/users`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${token}`
+      },
+      body: JSON.stringify(user)
+    });
+    
+    const data = await response.json();
+    console.log(data); // Para ver la respuesta del servidor en la consola
+
+    // Si la respuesta es exitosa, entonces redirige
+    if (response.ok) {
+    userLoging = user;
+    window.location.href = '../views/user.html';
+
+    return true;
+    } else {
+      console.error('Error al enviar los datos:', response.status);
+      showAlert('Error al enviar los datos', 'error', formulario);
+      const errorData = await response.json();
+      console.error('Mensaje de error:', errorData.message);
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status;
+  }
+}
+
+export const TypeEnum = {
+  Normal: 1,
+  Doctor: 2,
+  Administrador: 3,
+}
+
+export function typeOptionsSelected(selectElement, selectedType) {
+  //Limpia las opciones exisistentes
+  selectElement.innerHTML = '';
+
+  // Añade opciones basadas en genderEnum
+  for (const key in TypeEnum) {
+    const option = document.createElement('option');
+    option.value = TypeEnum[key];
+    option.textContent = key;
+    // Seleccionar la opción correspondiente
+    if (selectedType === key) {
+      option.selected = true;
+    }
+    selectElement.appendChild(option);
   }
 }
---
+++ ProyectoClinicaDental/apps/user.js
@@ -0,0 +1,164 @@
+import { isEmpty, showAlert, showSpinner, anyToken } from "./funciones.js";
+import {
+    getUsers,
+    postUser,
+    deleteUser,
+    TypeEnum
+} from "./API.js";
+import { User } from "./class.js";
+
+(function () {
+    const openModal = document.querySelector("#openModal");
+    const closeModal = document.querySelector("#closeModal");
+    const modal = document.querySelector("#myModal");
+    const list = document.querySelector("#user-list");
+    const formulario = document.querySelector("#formulario-user");
+    const main = document.querySelector("#main");
+    document.addEventListener("DOMContentLoaded", async () => {
+        document.addEventListener("click", confirmarEliminarUser);
+        // Diferentes consultas a la API como globales
+        const dataUser = await anyToken();
+        console.log(dataUser);
+        const users = await getUsers(dataUser);
+
+        async function printUser() {
+            users.forEach((user) => {
+                // Iteramos sobre el arreglo obtenido como respuesta
+
+                const {
+                    id_user,
+                    email,
+                    userName,
+                    type,
+                } = user;
+                // Scripting Time
+                const row = document.createElement("TR");
+                row.innerHTML += `
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${email} </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold">${userName}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                        <p class="text-gray-600">${type}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                        <a href="user-edit.html?id=${id_user}" data-user="${id_user}" class="text-teal-600 hover:text-teal-900 mr-5 editar">Editar</a>
+                        <a href="#" data-user="${id_user}" class="text-red-600 hover:text-red-900 mr-5 eliminar">Eliminar</a>
+                    </td>
+                `;
+
+                list.appendChild(row);
+            });
+        }
+
+        function typeOptions(selectElement) {
+            // Limpia las opciones existentes
+            selectElement.innerHTML = '';
+
+            // Añade una opción predeterminada vacía
+            const defaultOption = document.createElement('option');
+            defaultOption.value = '';
+            defaultOption.textContent = 'Selecciona un tipo';
+            selectElement.appendChild(defaultOption);
+
+            // Añade opciones basadas en TypeEnum
+            for (const key in TypeEnum) {
+                const option = document.createElement('option');
+                option.value = TypeEnum[key];
+                option.textContent = key;
+                selectElement.appendChild(option);
+            }
+        }
+      
+  
+      async function confirmarEliminarUser(e) {
+            if (e.target.classList.contains("eliminar")) {
+                const userDeleteId = parseInt(e.target.dataset.user);
+                console.log(userDeleteId);
+
+                const confirmar = confirm("¿Desea eliminar el registro?");
+                if (confirmar) {
+                    try {
+                        const exito = await deleteUser(dataUser, userDeleteId);
+
+                        if (exito) {
+                            console.log("Usuario eliminado correctamente");
+                            showAlert("Registro eliminado con exito", "Exito", main);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        } else {
+                            console.error("Error al eliminar al usuario");
+                            showAlert("Error al eliminar al usuario", "error", main);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        }
+                    } catch (error) {
+                        console.error("Error en la solicitud:", error);
+                        showAlert("Error en el servidor", "error", main);
+                        setTimeout(() => {
+                            location.reload();
+                        }, 2000);
+                    }
+                } else {
+                    console.log("No se eliminó");
+                }
+            }
+        }
+  
+      openModal.addEventListener("click", () => {
+            modal.classList.remove("hidden");
+            // Es para hacer mas fluida la animacion del modal
+            setTimeout(() => {
+                modal.children[0].classList.add("opacity-100", "scale-100");
+                typeOptions(document.querySelector("#type"));
+            }, 50);
+        });
+
+    closeModal.addEventListener("click", () => {
+        modal.children[0].classList.remove("opacity-100", "scale-100");
+        // Y esto tambien para hacer mas fluida la animacion para el modal
+        setTimeout(() => {
+            modal.classList.add("hidden");
+        }, 300);
+    });
+
+    formulario.addEventListener("submit", async (e) => {
+        e.preventDefault(); // Prevenir el evento de default que es POST
+        // Seleccionar todos los campos del form
+        const email = document.querySelector("#email").value;
+        const userName = document.querySelector("#userName").value;
+        const password = document.querySelector("#password").value;
+        const type = document.querySelector("#type").value;
+
+        const user = new User(
+            email,
+            password,
+            undefined,
+            userName,
+            type,
+        );
+
+        // Validar usuario
+        if (isEmpty(user)) {
+            // Si realmente hay un true, entonces esta vacia y haz lo siguiente
+            showAlert("¡Todos los campos son obligatorios!", "error", formulario);
+            return; // hasta aqui se termina la ejecucion
+        }
+        showSpinner(formulario);
+        // Hablando a la api
+        const response = await postUser(dataUser, user);
+        if (response) {
+            showAlert("Usuario Registrado Con Exito", "succes", formulario);
+            return;
+        } else {
+            showAlert("Error al enviar los datos", "error", formulario);
+            return;
+        }
+    });
+    printUser();
+})
+}) ();
